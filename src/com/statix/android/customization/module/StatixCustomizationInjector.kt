01fd2c2b57e3ce9310cad359e3fb6c96f1adc9f2
Anay Wadhera <anay1018@gmail.com>
2023-06-14 02:47:12 -0400

    StatixThemePicker: Update for tm-qpr3
    Change-Id: I49ffcd22e9853eb83bf27617d6ab7caf065961e6

--- src/com/statix/android/customization/module/StatixCustomizationInjector.kt
+++ src/com/statix/android/customization/module/StatixCustomizationInjector.kt
@@ -3,6 +3,7 @@ package com.statix.android.customization.module
 import android.app.Activity
 import android.content.Context
 
+import androidx.activity.ComponentActivity
 import androidx.fragment.app.Fragment
 
 import com.android.wallpaper.model.WallpaperInfo
@@ -10,6 +11,7 @@ import com.android.wallpaper.module.CustomizationSections
 import com.android.wallpaper.picker.MonetPreviewFragment
 
 import com.android.customization.module.ThemePickerInjector
+import com.android.customization.picker.notifications.ui.viewmodel.NotificationSectionViewModel
 
 public class StatixCustomizationInjector : ThemePickerInjector() {
 
@@ -25,11 +27,24 @@ public class StatixCustomizationInjector : ThemePickerInjector() {
         return MonetPreviewFragment.newInstance(wallpaperInfo, mode, viewAsHome, viewFullScreen, testingModeEnabled);
     }
 
-    override fun getCustomizationSections(activity: Activity): CustomizationSections {
+    override fun getCustomizationSections(activity: ComponentActivity): CustomizationSections {
         return customizationSections
             ?: StatixCustomizationSections(
+                    getColorPickerViewModelFactory(
+                        context = activity,
+                        wallpaperColorsViewModel = getWallpaperColorsViewModel(),
+                    ),
                     getKeyguardQuickAffordancePickerInteractor(activity),
-                    getKeyguardQuickAffordancePickerViewModelFactory(activity)
+                    getKeyguardQuickAffordancePickerViewModelFactory(activity),
+                    NotificationSectionViewModel.Factory(
+                        interactor = getNotificationsInteractor(activity),
+                    ),
+                    getFlags(),
+                    getClockCarouselViewModel(activity),
+                    getClockViewFactory(activity),
+                    getDarkModeSnapshotRestorer(activity),
+                    getThemedIconSnapshotRestorer(activity),
+                    getThemedIconInteractor(),
                 )
                 .also { customizationSections = it }
     }
