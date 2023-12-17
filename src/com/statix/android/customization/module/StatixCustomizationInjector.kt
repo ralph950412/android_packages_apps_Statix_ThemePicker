cbcfcce485c8e4b6167f48cdc0090f95a265a13e
Anay Wadhera <anay1018@gmail.com>
2023-12-16 23:55:07 -0500

    StatixThemePicker: update to udc qpr1
    Edits:
    Start using Hilt!!
    Remove WallpaperColorThemePreview and related classes
    Deprecated non-revamped UI fns
    
    Change-Id: I37fbc45f63fb629403861c51836641c991f7a6b4

--- src/com/statix/android/customization/module/StatixCustomizationInjector.kt
+++ src/com/statix/android/customization/module/StatixCustomizationInjector.kt
@@ -5,32 +5,34 @@ import androidx.activity.ComponentActivity
 import androidx.fragment.app.Fragment
 import com.android.customization.module.ThemePickerInjector
 import com.android.customization.picker.notifications.ui.viewmodel.NotificationSectionViewModel
-import com.android.wallpaper.model.WallpaperInfo
+import com.android.wallpaper.dispatchers.BackgroundDispatcher
+import com.android.wallpaper.dispatchers.MainDispatcher
 import com.android.wallpaper.module.CustomizationSections
-import com.android.wallpaper.picker.MonetPreviewFragment
+import javax.inject.Inject
+import javax.inject.Singleton
+import kotlinx.coroutines.CoroutineDispatcher
+import kotlinx.coroutines.CoroutineScope
 
-class StatixCustomizationInjector : ThemePickerInjector() {
+@Singleton
+open class StatixCustomizationInjector @Inject constructor(
+    @MainDispatcher mainScope: CoroutineScope,
+    @MainDispatcher mainDispatcher: CoroutineDispatcher,
+    @BackgroundDispatcher bgDispatcher: CoroutineDispatcher,
+) : ThemePickerInjector(
+    mainScope,
+    mainDispatcher,
+    bgDispatcher,
+) {
 
   private var customizationSections: CustomizationSections? = null
 
-  override fun getPreviewFragment(
-      context: Context,
-      wallpaperInfo: WallpaperInfo,
-      mode: Int,
-      viewAsHome: Boolean,
-      viewFullScreen: Boolean,
-      testingModeEnabled: Boolean
-  ): Fragment {
-    return MonetPreviewFragment.newInstance(
-        wallpaperInfo, mode, viewAsHome, viewFullScreen, testingModeEnabled)
-  }
-
   override fun getCustomizationSections(activity: ComponentActivity): CustomizationSections {
+    val wallpaperColorsViewModel = getWallpaperColorsViewModel()
     return customizationSections
         ?: StatixCustomizationSections(
                 getColorPickerViewModelFactory(
                     context = activity,
-                    wallpaperColorsViewModel = getWallpaperColorsViewModel(),
+                    wallpaperColorsViewModel = wallpaperColorsViewModel,
                 ),
                 getKeyguardQuickAffordancePickerInteractor(activity),
                 getKeyguardQuickAffordancePickerViewModelFactory(activity),
@@ -43,6 +45,7 @@ class StatixCustomizationInjector : ThemePickerInjector() {
                 getDarkModeSnapshotRestorer(activity),
                 getThemedIconSnapshotRestorer(activity),
                 getThemedIconInteractor(),
+                getColorPickerInteractor(activity, wallpaperColorsViewModel),
             )
             .also { customizationSections = it }
   }
--- src/com/statix/android/customization/picker/StatixCustomizationPickerApplication.kt
+++ src/com/statix/android/customization/picker/StatixCustomizationPickerApplication.kt
@@ -3,12 +3,18 @@ package com.statix.android.customization.picker
 import android.app.Application
 import com.android.wallpaper.module.InjectorProvider
 import com.statix.android.customization.module.StatixCustomizationInjector
+import dagger.hilt.android.HiltAndroidApp
+import javax.inject.Inject
 
-public class StatixCustomizationPickerApplication : Application() {
+@HiltAndroidApp(Application::class)
+class StatixCustomizationPickerApplication : Hilt_StatixCustomizationPickerApplication() {
+
+  @Inject
+  lateinit var injector: StatixCustomizationInjector
 
   override fun onCreate() {
     super.onCreate()
 
-    InjectorProvider.setInjector(StatixCustomizationInjector())
+    InjectorProvider.setInjector(injector)
   }
 }
